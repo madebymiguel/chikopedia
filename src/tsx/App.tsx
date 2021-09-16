3f521c0f554b3d034b642a3ff6c13155bff87ed4
miguel <74852114+mickeygcodes@users.noreply.github.com>
2021-09-15 21:02:18 -0700

    add eslint and related packages and a test component

--- src/tsx/App.tsx
+++ src/tsx/App.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "../scss/App.scss";
+import Test from "./Test";
 
 const App = () => {
   return (
     <div className="App">
       <div>Hello Chikopedia</div>
+      <Test />
     </div>
   );
 };
---
+++ src/tsx/Test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FunctionComponent } from "react";
+
+const Test: FunctionComponent = () => {
+  return (
+    <div>
+      <div>test</div>
+    </div>
+  );
+};
+
+export default Test;
