cc61dcc31d055fd32336d7dd6e79437fe81fb248
miguel <74852114+mickeygcodes@users.noreply.github.com>
2021-10-07 00:03:53 -0700

    wrong implementation for making a grid of pokemon. Pushing for now

--- src/components/PokemonGrid.tsx
+++ src/components/PokemonGrid.tsx
@@ -1,39 +1,50 @@
 import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 import { fetchPokemon } from "../apis/fetchPokemon";
 import { Pokemon } from "../types/Pokemon";
 import PokemonGridItem, { PokemonGridItemProps } from "./PokemonGridItem";
+import "../scss/PokemonGrid.scss";
 
 export default function PokemonGrid() {
-    const POKEMON_LIMIT = 2;
+  const POKEMON_LIMIT = 30;
 
-    const [pokemons, setPokemons] = useState<Pokemon>();
-    const getPokemon = (index: number) => {
-        const res = fetchPokemon(index);
-        res.then((pokemonObject) => {
-            setPokemons(pokemonObject);
-            console.log(pokemons);
-        });
-    };
+  const [pokemons, setPokemons] = useState<Pokemon>();
 
-    for(let i = 0; i < POKEMON_LIMIT; i++) {
-        getPokemon(i);
+  const getPokemon = (index: number) => {
+    const res = fetchPokemon(index);
+    res.then((pokemonObject) => {
+      setPokemons(pokemonObject);
+    });
+    gridItem();
+  };
+
+  console.log(pokemons);
+
+  const pokemonIteration = () => {
+    for (let i = 1; i < POKEMON_LIMIT; i++) {
+      getPokemon(i);
     }
+  };
 
+  const gridItem = () => {
+    const gridContainer = document.getElementById("inner");
+    const item = pokemons !== undefined && pokemons !== null && (
+      <PokemonGridItem
+        name={pokemons.name}
+        index={pokemons.id}
+        image={pokemons.sprites.front_default}
+      />
+    );
+    if (item) {
+      ReactDOM.render(item, gridContainer);
+    }
+  };
 
-    // useEffect(() => {
-    //     getPokemon(POKEMON_LIMIT);
-    // }, []);
-    
-    // do something with pokemons
-    
-    // first 1 - 30, second: 31- 60 ......
-    return(
-        <div id = "pokemon-grid">
-            Pokemon Grid
-            {/* <PokemonGridItem/> */}
-            <button onClick = {() => getPokemon(POKEMON_LIMIT)}>
-                Don't Click This!!
-            </button>
-        </div>
-    )
+  return (
+    <div id="pokemon-grid">
+      Pokemon Grid
+      <button onClick={() => pokemonIteration()}>Don't Click This!!</button>
+      <div id="inner"></div>
+    </div>
+  );
 }
--- src/components/PokemonGridItem.tsx
+++ src/components/PokemonGridItem.tsx
@@ -1,22 +1,22 @@
 import React from "react";
+import "../scss/PokemonGridItem.scss";
 
 export interface PokemonGridItemProps {
-    name: string;
-    index: number;
-    image: string;
+  name: string;
+  index: number;
+  image: string;
 }
 
-
 export default function PokemonGridItem({
-    name,
-    index,
-    image,
+  name,
+  index,
+  image,
 }: PokemonGridItemProps) {
-    return(
-        <div className = "grid-item-container">
-            <h3>{name}</h3>
-            <h4>{index}</h4>
-            <img src={image} />
-        </div>
-    )
+  return (
+    <div className="grid-item-container">
+      <h3>{name}</h3>
+      <h4>{index}</h4>
+      <img src={image} />
+    </div>
+  );
 }
