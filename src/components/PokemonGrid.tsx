d8877f355780f9e09cdcf950d620c2c98d76e87e
miguel <74852114+mickeygcodes@users.noreply.github.com>
2021-10-11 00:39:25 -0700

    almost there but still can't render pokemon grid

--- src/components/PokemonGrid.tsx
+++ src/components/PokemonGrid.tsx
@@ -3,21 +3,27 @@ import ReactDOM from "react-dom";
 import { fetchPokemon } from "../apis/fetchPokemon";
 import { fetchPokemonLimit } from "../apis/fetchPokemonLimit";
 import { Pokemon } from "../types/Pokemon";
-import { PokemonGridComponent} from "../types/PokemonGridComponent"
+import { PokemonGridComponent } from "../types/PokemonGridComponent";
 import { PokemonGridItem } from "../types/PokemonGridItem";
 import PokemonGridItems from "./PokemonGridItems";
 import "../scss/PokemonGrid.scss";
-import {PokemonLimit} from "../types/PokemonLimit"
+import { PokemonLimit } from "../types/PokemonLimit";
 
 export default function PokemonGrid() {
   const POKEMON_LIMIT = 10;
 
-  const [allPokemons, setAllPokemons] = useState<PokemonLimit>();
-  const [pokemonGrid, setPokemonGrid] = useState<PokemonGridComponent>();
- // const [pokemonGridItems, setPokemonGridItems] = useState<PokemonGridItems>();
+  // const [allPokemons, setAllPokemons] = useState<PokemonLimit>();
+  // const [pokemonGrid, setPokemonGrid] = useState<PokemonGridComponent>();
+  // const [pokemonGridItems, setPokemonGridItems] = useState<PokemonGridItems>();
 
-  const [pokemons, setPokemons] = useState<Pokemon>();
   const allPokemonData = [] as Pokemon[];
+  const [pokemons, setallPokemonData] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    getAllPokemon();
+    console.log(pokemons);
+    console.log("pokemons");
+  }, []);
 
   const getAllPokemon = () => {
     const res = fetchPokemonLimit(POKEMON_LIMIT);
@@ -25,24 +31,40 @@ export default function PokemonGrid() {
       allPokemon.results.forEach((pokemonLimitItem) => {
         const pokemonData = fetchPokemonData(pokemonLimitItem.url);
         pokemonData.then((pokemon) => {
+          // const tempPokemon = [...pokemons];
+          // console.log(tempPokemon);
+          // console.log("tempfirst^^^");
+          // tempPokemon.push(pokemon);
+          // setallPokemonData(tempPokemon);
+          // console.log(tempPokemon);
+          // console.log("tempsecond^^^");
+          // console.log(pokemons);
+          // console.log("pokemons^^^");
           allPokemonData.push(pokemon);
-        })
+        });
       });
-
     });
-    console.log(allPokemonData);
+    setallPokemonData(allPokemonData);
+    // console.log(allPokemonData);
   };
 
   const getPokemonGridResults = () => {
-    getAllPokemon();
-    console.log(allPokemonData);
-    allPokemonData.map((pokemon) => {
-      
-      return(
-        <PokemonGridItems name={pokemon.name} index={pokemon.id} image={pokemon.sprites.front_default} />
-      )
-    })
-  }
+    console.log("here");
+    console.log(pokemons);
+    console.log("here2");
+    const grid = pokemons.map((pokemon: Pokemon) => {
+      return (
+        <PokemonGridItems
+          name={pokemon.name}
+          index={pokemon.id}
+          image={pokemon.sprites.front_default}
+        />
+      );
+    });
+    console.log(grid);
+    console.log("grid^^^");
+    return grid;
+  };
 
   //const allPokemonGridData = getPokemonGridResults();
 
@@ -50,14 +72,15 @@ export default function PokemonGrid() {
     const res = await fetch(pokemonUrl);
     const pokemonResult: Pokemon = await res.json();
     return pokemonResult;
-  }
-
+  };
 
   return (
     <div id="pokemon-grid">
       Pokemon Grid
-      <button onClick={() => getPokemonGridResults()}>Don't Click This!!</button>
-      <div id="inner"></div>
+      {/* <button onClick={() => getPokemonGridResults()}>
+        Don't Click This!!
+      </button> */}
+      <div id="inner"> {getPokemonGridResults()}</div>
       {/* <PokemonGridComponent /> */}
     </div>
   );
