4d3a0db8ff611d9b5fef6c708b9a9ab2c683405d
miguel <74852114+mickeygcodes@users.noreply.github.com>
2021-11-24 23:16:36 -0800

    Fixed bug with POKEMON_LIMIT not working because of collision with replacePokemonNamesFromArray reading undefined

--- src/Variables/globalVariables.ts
+++ src/Variables/globalVariables.ts
@@ -1,3 +1,2 @@
 export const POKEMON_LIMIT = 898;
 export const MAX_POKEMON = 898;
-
--- src/utils/replacePokemonNamesFromArray.ts
+++ src/utils/replacePokemonNamesFromArray.ts
@@ -1,9 +1,12 @@
 import { pokemonNameExceptions } from "../Variables/pokemonNameExceptions";
 import { SimplePokemon } from "../types/SimplePokemon";
+import { POKEMON_LIMIT } from "../Variables/globalVariables";
 
 export default function replacePokemonName(pokemonArray: SimplePokemon[]) {
   pokemonNameExceptions.forEach((pokemon) => {
-    pokemonArray[pokemon.id - 1].name = pokemon.name.toLowerCase();
+    if (POKEMON_LIMIT > pokemon.id) {
+      pokemonArray[pokemon.id - 1].name = pokemon.name.toLowerCase();
+    }
   });
   return pokemonArray;
 }
