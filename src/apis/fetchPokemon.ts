8db856e49902cd4d1a102b882b8e0962a2381acc
warandstar <38151882+warandstar@users.noreply.github.com>
2021-12-05 15:41:19 -0800

    Make search compatible with lazy loading by enabling to search pokemon outside of session storage. There is some bug that if you type pokemon id instead of name, the last digit sometimes is omitted. maybe it is my laptop issue but might look into it later (#7)

--- src/apis/fetchPokemon.ts
+++ src/apis/fetchPokemon.ts
@@ -1,7 +1,12 @@
 import { Pokemon } from "../types/Pokemon";
 
 export async function fetchPokemon(index: string | number) {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${index}`);
-  const pokemonResult: Pokemon = await res.json();
-  return pokemonResult;
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${index}`);
+    const pokemonResult: Pokemon = await res.json();
+    return pokemonResult;
+  } catch (err) {
+    return {} as Pokemon;
+  }
+
 }
