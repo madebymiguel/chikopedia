5f9ecd52460ee329e9f1c9e6d7e66bd64a51b246
warandstar <andreakim123@naver.com>
2021-12-08 23:03:03 -0800

    play around the layoff for pokedex entry

--- src/components/PokedexEntry.tsx
+++ src/components/PokedexEntry.tsx
@@ -46,14 +46,14 @@ export default function PokedexEntry({
   weight,
   height,
   abilities,
-  genderRate,
+  genderRate, // genderRate = -1 means no gender
   captureRate,
   isLegendary,
   isMythical,
   growthRate,
   eggGroups,
-  color,
-  habitat,
+  color, // can be used for theme color for each pokemon
+  habitat, // can be used for background theme
   generation,
   evolutionChain,
   pokeball,
@@ -61,6 +61,7 @@ export default function PokedexEntry({
   livingDex,
 }: PokedexEntryProps) {
   return (
+    // the description parts can be refactored into react component
     <div className="pokedex-entry-container">
       <section className="pokedex-screen">
         <div className="header">
@@ -86,25 +87,57 @@ export default function PokedexEntry({
       <section className="pokedex-detailed-info">
         <PokemonTypes types={types} />
         <div className="description">
-          <span>WEIGHT: {(weight * 0.1).toFixed(1)} kg</span>
-          <span>HEIGHT: {(height * 0.1).toFixed(1)} m </span>
+          <div id="height">
+            <div id="height-title"> HEIGHT </div>
+            <div id="height-content"> {(height * 0.1).toFixed(1)} m </div>
+          </div>
+          <div id="weight">
+            <div id="weight-title"> WEIGHT </div>
+            <div id="weight-content"> {(weight * 0.1).toFixed(1)} kg </div>
+          </div>
         </div>
         <div className="extra">
-          <span>
-            Gender Rate: Male: ({100 - Math.round((genderRate / 8) * 100)}
-            %) Female: ({Math.round((genderRate / 8) * 100)}%)
-          </span>
-          <span>
-            Capture Rate: {captureRate} ({Math.round((captureRate / 255) * 100)}
-            %)
-          </span>
-          {isLegendary && <span> Legendary </span>}
-          {isMythical && <span> Mythical </span>}
-          <span>Growth Rate: {growthRate}</span>
-          <span>Egg Groups: {eggGroups}</span>
-          {/* <span>COLOR: {color} </span> */}
-          <span>HABITAT: {habitat} </span>
-          <span>GENERATION: {generation} </span>
+          <div className="rates">
+            <div id="gender-rate">
+              <div id="gender-rate-title"> GENDER</div>
+              <div id="gender-rate-content">
+                <div className="gender-rate-gender">
+                  <div className="gender-rate-title">Male</div>
+                  <div className="gender-rate-content">
+                    {100 - Math.round((genderRate / 8) * 100)}%
+                  </div>
+                </div>
+                <div className="gender-rate-gender">
+                  <div className="gender-rate-title">Female</div>
+                  <div className="gender-rate-content">
+                    {Math.round((genderRate / 8) * 100)}%
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div id="capture-rate">
+              <div id="capture-rate-title">Capture Rate</div>
+              <div id="capture-rate-content">
+                {captureRate} ({Math.round((captureRate / 255) * 100)}%)
+              </div>
+            </div>
+            <div id="growth-rate">
+              <div id="growth-rate-title"> Growth Rate </div>
+              <div id="growth-rate-content"> {growthRate} </div>
+            </div>
+          </div>
+          <div className="groups">
+            <div id="egg-groups">
+              <div id="egg-groups-title"> Egg Groups </div>
+              <div id="egg-groups-content"> {eggGroups} </div>
+            </div>
+            <div id="generation">
+              <div id="generation-title"> GENERATION </div>
+              <div id="generation-content"> {generation} </div>
+            </div>
+          </div>
+          {isLegendary && <div> Legendary </div>}
+          {isMythical && <div> Mythical </div>}
         </div>
         <PokemonStats stats={stats} />
         <PokemonAbilities abilities={abilities} />
