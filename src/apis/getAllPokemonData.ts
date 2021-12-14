0c27f0f9b7f08d75e29fcb4b98835421c686cf48
madebymiguel <74852114+madebymiguel@users.noreply.github.com>
2021-12-13 20:15:16 -0800

    Refactor toggle handle for menu (#11)
    Co-authored-by: miguel <74852114+mickeygcodes@users.noreply.github.com>

--- src/apis/getAllPokemonData.ts
+++ src/apis/getAllPokemonData.ts
@@ -1,12 +1,10 @@
 import { Pokemon } from "../types/pokemon/Pokemon";
 
 export default async function getAllPokemonData(index: number) {
-  const pokemonDataToFetch = [];
-
-  pokemonDataToFetch.push(fetch(`https://pokeapi.co/api/v2/pokemon/${index}`));
-  pokemonDataToFetch.push(
-    fetch(`https://pokeapi.co/api/v2/pokemon-species/${index}`)
-  );
+  const pokemonDataToFetch = [
+    fetch(`https://pokeapi.co/api/v2/pokemon/${index}`),
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${index}`),
+  ];
 
   const fetchedPokemonData = await Promise.all(pokemonDataToFetch);
   return await Promise.all(
