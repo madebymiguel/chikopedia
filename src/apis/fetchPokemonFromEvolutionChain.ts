f6aa551932713eaf4be31499c5968a22a8c953ab
warandstar <andreakim123@naver.com>
2021-12-20 22:01:15 -0800

    add comments and change naming in multiple files

--- src/apis/fetchPokemonFromEvolutionChain.ts
+++ src/apis/fetchPokemonFromEvolutionChain.ts
@@ -1,20 +1,25 @@
+// evolutionPathArray is 2d array and consist of 1st axis as possible paths of evolution chain of pokemon
+// and 2nd axis as pokemon in the possible path of evolution chain
+// For example, Wurmple has 2 paths of 1) wurmple -> silcoon -> beautifly 2) wurmple -> cascoon -> dustox
+// 1st element of 1st axis would be the first case above and 2nd element would be the second case above.
+
 export default async function fetchPokemonFromEvolutionChain(
-  pathArray: string[][]
+  evolutionPathArray: string[][]
 ) {
   const chainToBeFetched = [];
   for (
     let evolutionPath = 0;
-    evolutionPath < pathArray.length;
+    evolutionPath < evolutionPathArray.length;
     evolutionPath++
   ) {
     for (
       let pokemonInPath = 0;
-      pokemonInPath < pathArray[pokemonInPath].length;
+      pokemonInPath < evolutionPathArray[evolutionPath].length;
       pokemonInPath++
     ) {
       chainToBeFetched.push(
         fetch(
-          `https://pokeapi.co/api/v2/pokemon/${pathArray[evolutionPath][pokemonInPath]}`
+          `https://pokeapi.co/api/v2/pokemon/${evolutionPathArray[evolutionPath][pokemonInPath]}`
         )
       );
     }
--- src/utils/getSpritesFromEvolutionChain.ts
+++ src/utils/getSpritesFromEvolutionChain.ts
@@ -2,53 +2,24 @@ import { Chain } from "../types/evolutionChain/Chain";
 
 export function getSpritesFromEvolutionChain(
   chain: Chain,
-  chainArray: string[][],
+  evolutionPathArray: string[][],
   currentPath: string[]
 ) {
   currentPath.push(chain.species.name);
   if (chain.evolves_to.length === 0) {
     console.log(currentPath);
-    console.log(chainArray);
-    chainArray.push(currentPath);
+    console.log(evolutionPathArray);
+    evolutionPathArray.push(currentPath);
   }
 
   if (chain.evolves_to.length > 0) {
     for (let i = 0; i < chain.evolves_to.length; i++) {
       getSpritesFromEvolutionChain(
         chain.evolves_to[i],
-        chainArray,
+        evolutionPathArray,
         currentPath.slice(0, 2) // we would introduce height to make sure it parses properly with tree
       );
     }
   }
-  return chainArray;
+  return evolutionPathArray;
 }
-
-// if (evolutionChain.chain.evolves_to.length > 0) {
-//   chainArray.push(evolutionChain.chain.evolves_to[0].species.name);
-//   if (evolutionChain.chain.evolves_to[0].evolves_to.length > 0) {
-//     chainArray.push(
-//       evolutionChain.chain.evolves_to[0].evolves_to[0].species.name
-//     );
-//   }
-// }
-
-// {
-//   pokemon: wurmple,
-//   evolutions: [
-//     {pokemon: silcoon, evolutions: [{pokemon: beautifly}],
-//     {pokemon: duskoon, evolutions: [{pokemon: dustox}]}
-//   ]
-// }
-
-// export function getSpritesFromEvolutionChain({
-//   pokemon,
-//   evolutions,
-// } : PokemonEvolutionTreeNode
-// ): PokemonEvolutionTreeNode {
-//   if (chain.evolves_to.length === 0) {
-//     return { pokemon: chain.species.name, evolutions: null };
-//   }
-//   return { pokemon: chain.species.name, evolutions:
-//     getSpritesFromEvolutionChain(chain.evolves_to, chainArray);
-//   };
