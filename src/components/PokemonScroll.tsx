8c3e3c33369378b4b15ccb69149738c3a2a78039
warandstar <andreakim123@naver.com>
2022-01-14 22:41:37 -0800

    Implemented pokedex-like scroll feature; TODO: Go back button should bring you to previous pokemon spot

--- src/components/PokemonScroll.tsx
+++ src/components/PokemonScroll.tsx
@@ -1,5 +1,11 @@
+import { useMemo, useState } from "react";
+import { Link } from "react-router-dom";
+import ArrowUpward from "../assets/arrow-upward.svg";
+import ArrowDownward from "../assets/arrow-downward.svg";
 import "../scss/PokemonScroll.scss";
 import { SimplePokemon } from "../types/SimplePokemon";
+import PokemonScrollElement from "./PokemonScrollElement";
+import { MAX_POKEMON } from "../variables/globalVariables";
 
 export interface PokemonScrollProps {
   isLoading: boolean;
@@ -12,10 +18,88 @@ export default function PokemonScroll({
   allPokemon,
   livingDex,
 }: PokemonScrollProps) {
+  // display 7 pokemon list (one main, 3 prev and 3 next pokemon)
+  // show only the main pokemon sprite
+  // arrow will switch the main pokemon as well as the pokemon previous and next to main
+
+  // moveUp
+  // moveDown
+  // generateItem
+
+  // need to keep track of index
+  // for loop should look index - 3 to index + 3
+
+  const MAX_SCROLL_POKEMON = 7;
+
+  const [currentIndex, setCurrentIndex] = useState(1);
+
+  const currentSprite = allPokemon[currentIndex - 1].sprite;
+
+  const pokemonScrollItems = useMemo(() => {
+    if (isLoading) {
+      return [];
+    }
+    const pokemonInScroll: SimplePokemon[] = [];
+
+    for (
+      let i = currentIndex - Math.round(MAX_SCROLL_POKEMON / 2);
+      i < currentIndex + Math.floor(MAX_SCROLL_POKEMON / 2);
+      i++
+    ) {
+      if (i >= 0 && i <= MAX_POKEMON) {
+        const currentPokemon = allPokemon[i];
+        pokemonInScroll.push(currentPokemon);
+      }
+    }
+
+    return pokemonInScroll.map((pokemonObject: SimplePokemon) => {
+      return (
+        <PokemonScrollElement
+          key={pokemonObject.id}
+          name={pokemonObject.name}
+          index={pokemonObject.id}
+          livingDex={livingDex}
+          active={currentIndex}
+          setCurrentIndex={setCurrentIndex}
+        />
+      );
+    });
+  }, [allPokemon, currentIndex, isLoading]);
+
   return (
     <div className="pokemon-scroll-container">
-      <div className="pokemon-scroll">
-        <h2 className="title">Coming Soon!</h2>
+      <div className="sprite">
+        <Link to={`/pokemon/${currentIndex}`}>
+          <img
+            src={currentSprite}
+            className="sprite-image"
+            alt="pokemon-sprite"
+          ></img>
+        </Link>
+      </div>
+
+      <div className="pokemon-scroll-carousel">
+        <div className="prev">
+          {currentIndex > MAX_SCROLL_POKEMON && (
+            <img
+              src={ArrowUpward}
+              alt="up-arrow"
+              className="arrow-image"
+              onClick={() => setCurrentIndex(currentIndex - MAX_SCROLL_POKEMON)}
+            />
+          )}
+        </div>
+        <div className="pokemon-scroll">{pokemonScrollItems}</div>
+        <div className="next">
+          {currentIndex < MAX_POKEMON - MAX_SCROLL_POKEMON && (
+            <img
+              src={ArrowDownward}
+              alt="down-arrow"
+              className="arrow-image"
+              onClick={() => setCurrentIndex(currentIndex + MAX_SCROLL_POKEMON)}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/PokemonScrollElement.tsx
@@ -0,0 +1,62 @@
+import greyPokeball from "../assets/grey-pokeball.svg";
+import redPokeball from "../assets/red-pokeball.svg";
+import upperCaseFirstLetter from "../utils/upperCaseFirstLetter";
+import { handlePokeballColorChange } from "../utils/handlePokeballColorChange";
+import { getLivingDexSet } from "../utils/getLivingDexSet";
+import formatIndex from "../utils/formatIndex";
+import { useState } from "react";
+import "../scss/PokemonScrollElement.scss";
+
+export interface PokemonScrollElementProps {
+  name: string;
+  index: number;
+  livingDex: boolean;
+  active: number;
+  setCurrentIndex: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export default function PokemonScrollElement({
+  name,
+  index,
+  livingDex,
+  active,
+  setCurrentIndex,
+}: PokemonScrollElementProps) {
+  const livingDexStorage = getLivingDexSet();
+
+  const [pokeball, setPokeball] = useState(
+    livingDexStorage[index] ? redPokeball : greyPokeball
+  );
+
+  const level = Math.abs(active - index);
+
+  const containerClassName = "scroll-element-container level" + level;
+  return (
+    <div className={containerClassName}>
+      <div className="pokeball-container">
+        {livingDex && (
+          <input
+            type="image"
+            src={pokeball}
+            alt="pokeball"
+            className="pokeball"
+            onClick={() => {
+              if (livingDex) {
+                handlePokeballColorChange({ pokeball, index, setPokeball });
+              }
+            }}
+          />
+        )}
+      </div>
+      <div
+        className="scroll-element-content"
+        onClick={() => {
+          setCurrentIndex(index);
+        }}
+      >
+        <span className="pokemon-index">#{formatIndex(index)}</span>
+        <h2 className="pokemon-name">{upperCaseFirstLetter(name)}</h2>
+      </div>
+    </div>
+  );
+}
